f379c258c0935439348a09c1e688e4ecf4132530
Dan <dpmckeon@gmail.com>
2016-06-15 13:59:53 -0700

    Changed names from Cancer Resources to Cancer Research Connect

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,19 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Cancer Resources"
+    assert_select "title", "Cancer Research Connect"
   end
 
   test "should get about" do
   	get :about
   	assert_response :success
-    assert_select "title", "About | Cancer Resources"
+    assert_select "title", "About | Cancer Research Connect"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Contact | Cancer Resources"
+    assert_select "title", "Contact | Cancer Research Connect"
   end
 
 end
